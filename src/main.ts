a642983823f461d1d991bebf22e8e0f9cd9ba8fb
Dedy Rizaldi <dedyrizaldi03@gmail.com>
2026-08-15 02:15:27 +0700

    update alogritm vessel position 3

--- src/main.ts
+++ src/main.ts
@@ -143,6 +143,7 @@ async function bootstrap() {
     app.enableCors({
       origin: [
         'http://localhost:3001',
+        'http://192.168.1.7:3001',
         'http://127.0.0.1:3001',
         'https://ais.becta.co.id',
       ],
